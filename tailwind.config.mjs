9c14ce7c7feee68f2099331edbc503e00d23e1e1
Jakob Rössner <jakob@lylu.de>
2024-07-31 19:51:11 +1000

    feat: added theme + basic navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'text': 'var(--text)',
+				'background': 'var(--background)',
+				'primary': 'var(--primary)',
+				'secondary': 'var(--secondary)',
+				'accent': 'var(--accent)',
+			},
+		},
 	},
 	plugins: [],
 }
