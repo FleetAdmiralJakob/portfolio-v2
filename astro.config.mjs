5203f24215397d94298d61f271a0fb9e49ad5445
Jakob Rössner <jakob.roessner@outlook.de>
2024-12-04 02:21:21 +1100

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import metaTags from "astro-meta-tags";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [metaTags(), tailwind(), sitemap()],
-  output: 'static',
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
     imageService: true,
-  })
+  }),
+  site: "https://roessner.tech",
 });
